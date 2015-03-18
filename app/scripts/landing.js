84623cccafc6db88f37d0cd47a70ea6abba86e57
Kateryna <zbsht15@gmail.com>
2015-03-18 21:05:19 +0000

    Used jQuery to populate the collection view.

--- app/scripts/landing.js
+++ app/scripts/landing.js
@@ -4,6 +4,22 @@ $(document).ready(function() {
 		$(this).text(subText + "!!!");
 	});
 
+
+  //var onHover = function(event) {
+    //console.log('Hover action triggered.');
+    //$(this).animate({'color: rgb(#AA3939)'});
+  //};
+
+  //var offHover = function(event) {
+    //console.log('Hover action triggered.');
+    //$(this).animate({'color: rgb(#AAA593)'});
+  //};
+
+    //$('.hero-content h3').hover(onHover, offHover); 
+    
+  //$('.player-header-nav navbar')
+
+
 var onHoverAction = function(event) {
      console.log('Hover action triggered.');
      $(this).animate({'margin-top': '10px'});
